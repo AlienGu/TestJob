8bc56bb88ebfbd92458c7b17766af11c3e2e9c03
AlienGu <guguoqiang618@163.com>
2018-06-20 11:07:52 +0800

    初始添加Xcode工程

---
+++ TestJob/TestJob/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestJob
+//
+//  Created by Alien on 2018/6/20.
+//  Copyright © 2018年 Alien. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
